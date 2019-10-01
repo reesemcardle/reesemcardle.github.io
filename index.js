914d1c9bd2826e819ce3f8b008c4d82c3b090a49
Reese McArdle <reese@reesemcrdlesMBP.fios-router.home>
2019-09-30 21:32:28 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
  var gifList = [
-                "gifs/gif1.gif",
-                "gifs/gif2.gif",
-                "gifs/gif3.gif",
-                "gifs/gif4.gif",
-                "gifs/gif5.gif",
-                "gifs/gif6.gif",]
+                "https://github.com/reesemcardle/reesemcardle.github.io/blob/master/gifs/gif1.gif?raw=true",
+                "https://github.com/reesemcardle/reesemcardle.github.io/blob/master/gifs/gif2.gif?raw=true",
+                "https://github.com/reesemcardle/reesemcardle.github.io/blob/master/gifs/gif3.gif?raw=true",
+                "https://github.com/reesemcardle/reesemcardle.github.io/blob/master/gifs/gif4.gif?raw=true",
+                "https://github.com/reesemcardle/reesemcardle.github.io/blob/master/gifs/gif5.gif?raw=true",
+                "https://github.com/reesemcardle/reesemcardle.github.io/blob/master/gifs/gif6.gif?raw=true",]
 
                 var rand = Math.floor(Math.random() * 6)
 var element = document.getElementById("myGif");
