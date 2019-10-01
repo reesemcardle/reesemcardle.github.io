592ebde61c36a617f609cf6286b408e8ae8d0eba
Reese McArdle <reese@reesemcrdlesMBP.fios-router.home>
2019-09-30 22:28:08 -0400

    v1.2

--- index.js
+++ index.js
@@ -4,9 +4,18 @@
                 "https://github.com/reesemcardle/reesemcardle.github.io/blob/master/gifs/gif3.gif?raw=true",
                 "https://github.com/reesemcardle/reesemcardle.github.io/blob/master/gifs/gif4.gif?raw=true",
                 "https://github.com/reesemcardle/reesemcardle.github.io/blob/master/gifs/gif5.gif?raw=true",
-                "https://github.com/reesemcardle/reesemcardle.github.io/blob/master/gifs/gif6.gif?raw=true",]
+                "https://github.com/reesemcardle/reesemcardle.github.io/blob/master/gifs/gif6.gif?raw=true",
+                "https://github.com/reesemcardle/reesemcardle.github.io/blob/master/gifs/gif7.gif?raw=true",
+                "https://github.com/reesemcardle/reesemcardle.github.io/blob/master/gifs/gif8.gif?raw=true",
+                "https://github.com/reesemcardle/reesemcardle.github.io/blob/master/gifs/gif9.gif?raw=true",
+                "https://github.com/reesemcardle/reesemcardle.github.io/blob/master/gifs/gif10.gif?raw=true",
+                "https://github.com/reesemcardle/reesemcardle.github.io/blob/master/gifs/gif11.gif?raw=true",
+                "https://github.com/reesemcardle/reesemcardle.github.io/blob/master/gifs/gif12.gif?raw=true",
+                "https://github.com/reesemcardle/reesemcardle.github.io/blob/master/gifs/gif13.gif?raw=true",
+                "https://github.com/reesemcardle/reesemcardle.github.io/blob/master/gifs/gif14.gif?raw=true",
+                "https://github.com/reesemcardle/reesemcardle.github.io/blob/master/gifs/gif15.gif?raw=true"]
 
-                var rand = Math.floor(Math.random() * 6)
+                var rand = Math.floor(Math.random() * 15)
 var element = document.getElementById("myGif");
 element.src = gifList[rand]
 console.log("yooo")
